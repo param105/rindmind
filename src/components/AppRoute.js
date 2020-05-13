d8e61a992cd84e74a84991ed5ab883c182c66201
Parmeshwar <parmeshwar@bruviti.com>
2020-05-13 19:46:08 +0530

    Material components added

---
+++ src/components/AppRoute.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import '../css/App.css';
+import Home from '../components/home/js/Home.js';
+import About from '../components/about/js/About.js';
+import Training from '../components/training/js/Training.js';
+import DashBoard from '../components/dashboard/js/Dashboard.js';
+
+export default function AppRoute() {
+    return (      
+             <HashRouter>
+          <nav class="myNavBar">
+            <div>
+              <header class="App-header"> <span>R</span>ind<span>M</span>ind </header>
+              <div class="tagline"> The Spiritual Technocrat </div>
+            </div>
+            <div>
+              <ul >
+                <li><NavLink to="/about"> about </NavLink></li>
+                <li><NavLink to="/training"> Training </NavLink></li>
+                <li><NavLink to="/dashboard"> DashBoard </NavLink></li>
+                <li><NavLink to="/"> Home </NavLink></li>
+              </ul>
+            </div>
+          </nav>
+          <div className="content">
+            <Route path="/training" component={Training} />
+            <Route path="/about" component={About} />
+            <Route exact path="/dashboard" component={DashBoard} />
+            <Route exact path="/" component={Home} />
+          </div>
+        </HashRouter>
+       
+    )
+}
---
+++ src/components/dashboard/js/ResponsiveDrawer.js
@@ -0,0 +1,192 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuList from '@material-ui/core/MenuList';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import NewReleases from '@material-ui/icons/NewReleases'
+import Home from '../../home/js/Home'
+import Training from '../../training/js/Training';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9) + 1,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 0),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(0),
+    },
+    link:{
+        textDecoration : 'none',
+        color : theme.palette.text.primary
+    }
+}));
+
+export default function ResponsiveDrawer() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Router>
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    className={clsx(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}
+                >
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={handleDrawerOpen}
+                            edge="start"
+                            className={clsx(classes.menuButton, {
+                                [classes.hide]: open,
+                            })}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <div>
+                            <Typography variant="h4">
+                                <span>R</span>ind<span>M</span>ind
+              </Typography>
+                            <Typography color="textSecondary" variant="h6">
+                                The Spiritual Technocrat
+              </Typography>
+
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    className={clsx(classes.drawer, {
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    })}
+                    classes={{
+                        paper: clsx({
+                            [classes.drawerOpen]: open,
+                            [classes.drawerClose]: !open,
+                        }),
+                    }}
+                >
+                    <div className={classes.toolbar}>
+                        <IconButton onClick={handleDrawerClose}>
+                            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <Link to="/" className = {classes.link}>
+                        <MenuList >
+                            <MenuItem button key="News">
+                                <ListItemIcon>{< NewReleases />} </ListItemIcon>
+                                <ListItemText primary="News" />
+                            </MenuItem>
+                        </MenuList>
+                    </Link>
+                    <Divider />
+                    <Link to="/training" className = {classes.link}>
+                    <MenuList>
+                        <MenuItem button key="Training">
+                            <ListItemIcon>{< MailIcon />} </ListItemIcon>
+                            <ListItemText primary="Training" />
+                        </MenuItem>
+                    </MenuList>
+                    </Link>
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+
+                        <Switch>
+                            <Route exact path ="/">
+                                <Home/>
+                            </Route>
+                            <Route exact path ="/training">
+                                <Training/>
+                            </Route>
+                        </Switch>
+
+                </main>
+            </div>
+        </Router>
+    );
+}
